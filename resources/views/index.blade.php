8da4328176615fe5a3ab01bb0986e652cad09253
Anwarbel1 <anwarbelhassan@gmail.com>
2023-02-07 14:10:56 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel 9 vite with react</title>
+
+        @viteReactRefresh
+        @vite('resources/js/index.jsx')
+        @vite( [ 'resources/sass/app.scss',
+        'resources/js/app.js',
+    ])
+    </head>
+    <body>
+        <div id="app"></div>
+    </body>
+</html>
