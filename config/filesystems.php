38d73ed530f7656e30fcbecda8699f217a7a2429
BelAnouar <anwarbelhassan@example.com>
2025-09-10 17:30:13 +0100

    Refactor code structure for improved readability and maintainability

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,7 +54,16 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
-        ],
+        ],'minio' => [
+    'driver' => 's3',
+    'key' => env('MINIO_ACCESS_KEY'),
+    'secret' => env('MINIO_SECRET_KEY'),
+    'endpoint' => env('MINIO_ENDPOINT'),
+    'region' => 'us-east-1',
+    'bucket' => env('MINIO_BUCKET'),
+    'use_path_style_endpoint' => true,
+],
+
 
     ],
 
