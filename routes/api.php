08de09e442dd5666a96203d374e570faf2b33f66
BelAnouar <anwarbelhassan@example.com>
2025-09-10 17:33:26 +0100

    Add API routes for dashboard statistics, categories, and applications

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,10 @@ use App\Http\Controllers\Api\FileController;
 use App\Http\Controllers\Api\PayementController;
 use App\Http\Controllers\Api\UsersController;
 use App\Http\Controllers\Api\WeeksController;
+use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProgramController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,9 +32,33 @@ Route::middleware('auth:sanctum')->group(function(){
    Route::apiResource('/users', UsersController::class);
    Route::apiResource("/cours",CoursController::class);
    Route::apiResource("/weeks",WeeksController::class);
-   Route::apiResource("/payemment",PayementController::class);
+  Route::post('/create-payment-intent', [PayementController::class, 'createPaymentIntent']);
+  Route::post('/payment-success', [PayementController::class, 'handleSuccess']);
+      Route::get('/check-course-access/{courseId}', [PayementController::class, 'checkCourseAccess']);
+    
    Route::apiResource("/File",FileController::class);
-   
+   Route::get('/files/week/{idWeek}', [FileController::class, 'getFilesByWeek']);
+
+   Route::get('/dashboard/stats', [DashboardController::class, 'getStatistics']);
+    Route::get('/dashboard/user-growth', [DashboardController::class, 'getUserGrowth']);
+    Route::get('/dashboard/course-popularity', [DashboardController::class, 'getCoursePopularity']);
+    Route::get('/dashboard/revenue', [DashboardController::class, 'getRevenue']);
+    Route::get('/dashboard/recent-activity', [DashboardController::class, 'getRecentActivity']);  Route::controller(ProgramController::class)->prefix('programs')->name('programs.')->group(function () {
+      
+        Route::get('/', 'index')->name('index');
+        
+       
+        Route::post('/', 'store')->name('store');
+        
+       
+        Route::get('/{program}', 'show')->name('show')->where('program', '[0-9]+');
+        
+     
+        Route::put('/{program}', 'update')->name('update')->where('program', '[0-9]+');
+        
+    
+        Route::delete('/{program}', 'destroy')->name('destroy')->where('program', '[0-9]+');
+    });
 });
 
 
@@ -41,3 +69,51 @@ Route::get("/login",[AuthController::class,"islogin"])->name("login");
 
 
 
+  
+    
+
+    Route::controller(CategoryController::class)->prefix('categories')->name('categories.')->group(function () {
+       
+        Route::get('/', 'index')->name('index');
+        
+
+        Route::post('/', 'store')->name('store');
+        
+       
+        Route::get('/popular', 'popular')->name('popular');
+    
+        Route::get('/with-programs', 'withPrograms')->name('with-programs');
+        
+       
+        Route::get('/statistics', 'statistics')->name('statistics');
+
+        Route::get('/{category}', 'show')->name('show')->where('category', '[0-9]+');
+       
+        Route::put('/{category}', 'update')->name('update')->where('category', '[0-9]+');
+    
+        Route::delete('/{category}', 'destroy')->name('destroy')->where('category', '[0-9]+');
+    });
+    
+   
+    Route::controller(ApplicationController::class)->prefix('applications')->name('applications.')->group(function () {
+       
+        Route::get('/', 'index')->name('index');
+        
+  
+        Route::post('/', 'store')->name('store');
+       
+        Route::get('/statistics', 'statistics')->name('statistics');
+
+        Route::get('/{application}', 'show')->name('show')->where('application', '[0-9]+');
+     
+        Route::put('/{application}', 'update')->name('update')->where('application', '[0-9]+');
+        
+        
+        Route::put('/{application}/approve', 'approve')->name('approve')->where('application', '[0-9]+');
+        
+    
+        Route::put('/{application}/reject', 'reject')->name('reject')->where('application', '[0-9]+');
+        
+        Route::delete('/{application}', 'destroy')->name('destroy')->where('application', '[0-9]+');
+    });
+    
